f555c3105643ed18671f4c60bd0f27f21526e8c5
Davide Cesare <davidereaper@gmail.com>
2020-09-02 17:08:43 +0200

    Impostazione commenti e "keydown"
    secondo bonus

--- js/main.js
+++ js/main.js
@@ -3,52 +3,58 @@ $(document).ready(function (){
   var nClick = $(".next");
   var pClick = $(".prev");
 
+  // 1.1 Impostazione click previous img.
+  // immagine e pallino scorrono verso sinistra quando si preme la freccia (fa-angle-left)
   pClick.click(
     function () {
       var imgActv = $("img.active");
       imgActv.removeClass("active");
-      if (imgActv.hasClass("first")) {
-        $("img.last").addClass("active");
-      } else {
-        imgActv.prev().addClass("active");
-      }
-
       var saircol = $("i.active");
       saircol.removeClass("active");
-      if (saircol.hasClass("first")) {
+
+      if (imgActv.hasClass("first") && saircol.hasClass("first")) {
+        $("img.last").addClass("active");
         $("i.last").addClass("active");
       } else {
+        imgActv.prev().addClass("active");
         saircol.prev().addClass("active");
       }
     }
   );
 
+  // 1.2 Impostazione click next img.
+  // immagine e pallino scorrono verso destra quando si preme la freccia (fa-angle-right)
   nClick.click(
     function () {
       var imgActv = $("img.active");
       imgActv.removeClass("active");
-      if (imgActv.hasClass("last")) {
-        $("img.first").addClass("active");
-      } else {
-        imgActv.next().addClass("active");
-      }
-
       var saircol = $("i.active");
       saircol.removeClass("active");
-      if (saircol.hasClass("last")) {
+
+      if (imgActv.hasClass("last") && saircol.hasClass("last")) {
+        $("img.first").addClass("active");
         $("i.first").addClass("active");
       } else {
+        imgActv.next().addClass("active");
         saircol.next().addClass("active");
       }
     }
   );
 
-
-
-  $("nav>i").click(
-    function () {
-      var clickSaircol = $("i.fa-circle");
-      clickSaircol.addClass("active");
+  // 2.1 Impostazione keydown previous img
+  // immagine e pallino scorrono verso sinistra quando si preme il tasto "freccia sinistra" sulla tastiera
+  $(document).keydown(function(e){
+    if (e.keyCode == 37) {
+      pClick.click();
     }
-  );
+  });
+
+  // 2.2 Impostazione keydown previous img
+  // immagine e pallino scorrono verso destra quando si preme il tasto "freccia destra" sulla tastiera
+  $(document).keydown(function(e){
+    if (e.keyCode == 39) {
+      nClick.click();
+    }
+  });
+
 });
